a52c63fd8f2a1221109f00a50c4d013bcc67ce6d
Vishal Soraganvi <99650261+VishalSoraganvi@users.noreply.github.com>
2023-08-31 17:19:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { StarIcon } from "lucide-react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="outer">
+        <div className="box">
+          <div className="star">
+            <StarIcon fill="orange" size={20} className="starIcon" />
+          </div>
+          <div className="text">
+            <h1>How did we do?</h1>
+            <p>
+              Please let us know how we did with your support request. All
+              feedback is appreciated to help us improve our offering!
+            </p>
+          </div>
+          <div className="rate">
+            <div className="rate1">
+              <div className="ratings">1</div>
+            </div>
+            <div className="rate2">
+              <div className="ratings">2</div>
+            </div>
+            <div className="rate3">
+              <div className="ratings">3</div>
+            </div>
+            <div className="rate4">
+              <div className="ratings">4</div>
+            </div>
+            <div className="rate5">
+              <div className="ratings">5</div>
+            </div>
+          </div>
+          <button>SUBMIT</button>
+        </div>
+      </div>
     </div>
   );
 }
